081e8ec18db236be3783f66a0c261c2e9b91eed9
tevon walker <tevjwalker@gmail.com>
2024-12-02 16:39:04 -0500

    Add setup packaging file

---
+++ tweet_crud/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+  
+setup( 
+    name='tweet_crud', 
+    version='1.0', 
+    description='A simple crud app replicating a simplistic tweet service', 
+    author='Tevon Walker',  
+    packages= find_packages()
+) 
