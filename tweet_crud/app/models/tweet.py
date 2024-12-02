9f4bad3d3ebc2269d4398368ac0f23bddec8a5d3
tevon walker <tevjwalker@gmail.com>
2024-12-02 16:40:06 -0500

    Move tweet model

---
+++ tweet_crud/app/models/tweet.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel, Field
+from typing import List, Optional
+from datetime import datetime
+
+class Tweet(BaseModel):
+    tweet_id: str=None
+    tweet_content: str
+    likes: Optional[List[str]]
+    mentions: Optional[List[str]]
+    hashtags: Optional[List[str]]
+    created_at: datetime = Field(default_factory=datetime.now)
+    user_id: str
+    username: str
