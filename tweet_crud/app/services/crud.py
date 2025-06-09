59269f30b255dd20560891efa0c29558b2b5dcf6
tevon walker <tevjwalker@gmail.com>
2025-06-09 18:39:17 -0400

    Add likes functionality

--- tweet_crud/app/services/crud.py
+++ tweet_crud/app/services/crud.py
@@ -2,13 +2,16 @@ from app.db.models import TweetDocument
 from typing import List, Dict
 import json
 
-def create_tweet(tweet_content=str, username=str, user_id=str, mentions:List[str]=None, hashtags:List[str]=None) -> str:
+def create_tweet(tweet_content=str, username=str, user_id=str, media_url:str=None, mentions:List[str]=None, hashtags:List[str]=None) -> str:
     
     new_tweet = TweetDocument(tweet_content=tweet_content,
                               username=username,
                               user_id=user_id,
                               mentions=mentions,
-                              hashtags=hashtags)
+                              hashtags=hashtags,
+                              media_url=media_url)
+    
+    print (new_tweet.to_json())
     
     return str(new_tweet.save().id)
 
@@ -41,7 +44,7 @@ def get_tweets() -> List[Dict]:
 
     tweets = []
     
-    for tweet in TweetDocument.objects():
+    for tweet in TweetDocument.objects().order_by('-created_at'):
         tweets.append(json.loads(tweet.to_json()))
 
     return tweets
@@ -63,3 +66,31 @@ def get_tweets_of_user(user_id: str) -> List[Dict]:
         tweets.append(json.loads(tweet.to_json()))
 
     return tweets
+
+def add_like_to_tweet(tweet_id: str, user_id: str) -> bool:
+    target_tweet = None
+    
+    for tweet in TweetDocument.objects(id=tweet_id):
+        target_tweet = tweet
+        
+    if target_tweet:
+        # Check if user already liked the tweet
+        if user_id not in target_tweet.likes:
+            target_tweet.update(push__likes=user_id)
+            return True
+    
+    return False
+
+def remove_like_from_tweet(tweet_id: str, user_id: str) -> bool:
+    target_tweet = None
+    
+    for tweet in TweetDocument.objects(id=tweet_id):
+        target_tweet = tweet
+        
+    if target_tweet:
+        # Check if user has liked the tweet
+        if user_id in target_tweet.likes:
+            target_tweet.update(pull__likes=user_id)
+            return True
+    
+    return False
