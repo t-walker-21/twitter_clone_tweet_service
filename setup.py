fc1cf97ca6f0cd2d211df68d760bfa5b13025311
tevon walker <tevjwalker@gmail.com>
2025-06-09 20:25:30 -0400

    Remove setup file

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-  
-setup( 
-    name='tweet_crud', 
-    version='1.0', 
-    description='A simple crud app replicating a simplistic tweet service', 
-    author='Tevon Walker',  
-    packages= find_packages(where='src'),
-) 
